dc7b6db00e032f50cf213f3406585b6caf735b0f
David Oliveira <david.deoliveirajunior@outlook.com>
2021-03-28 14:12:56 -0300

    ajustando a logica

--- br/com/treinamento/ComparaTipos.java
+++ br/com/treinamento/ComparaTipos.java
@@ -2,7 +2,9 @@ package br.com.treinamento;
 
 public class ComparaTipos {
     public static void main(String[] args){
-        String nome = "DAvid";
+        // String nome = "DAvid";
+        // String nome = null;
+        String nome = new String();
         if(nome instanceof String) {
             System.out.println("e String");
         } else {

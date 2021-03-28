2175280b63b0c83299a761a12b9a554d8a212a2c
David Oliveira <david.deoliveirajunior@outlook.com>
2021-03-28 15:46:30 -0300

    Media alunos dinamica, alunos infinitos

---
+++ br/com/treinamento/MediaAlunosDinamica.java
@@ -0,0 +1,34 @@
+package br.com.treinamento;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class MediaAlunosDinamica {
+    public static void main(String[] args){
+        float notaAluno = 0;
+        float totalNota = 0;
+        float mediaNota = 0;
+        int totalAluno = 0;
+
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+
+        while (true) {
+            System.out.print("Insira um nota para continuar, ou '-1' para terminar o programa: ");
+            notaAluno = entrada.nextFloat();
+            if (notaAluno < 0){
+                break;
+            }
+            totalNota += notaAluno;
+            totalAluno++;
+        }
+        entrada.close();
+        if (totalNota == 0){
+            System.out.println("nenhuma nota foi inserida!");
+        }else {
+        mediaNota = totalNota / totalAluno;
+        System.out.println("a nota media da sala, para " + totalAluno +" alunos, foi de " + mediaNota);
+        }
+    }
+    
+}

5e07c0bc351379ddec76e87a0da273b53c611391
David Oliveira <david.deoliveirajunior@outlook.com>
2021-03-28 14:22:32 -0300

    Operadores logicos

---
+++ br/com/treinamento/Operadores.java
@@ -0,0 +1,16 @@
+package br.com.treinamento;
+
+public class Operadores {
+    public static void main(String[] args){
+
+        int num1 = 10;
+        int num2 = 20;
+
+        if (num1 == 9 || num2 == 20){
+            System.out.println("True");
+        } else{
+            System.out.println("False");
+        }
+    }
+    
+}

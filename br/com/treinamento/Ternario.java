e55719fca9e8c1faed2459e71dfb1a4e01df19f0
David Oliveira <david.deoliveirajunior@outlook.com>
2021-03-28 14:32:43 -0300

    Operadores Ternario

---
+++ br/com/treinamento/Ternario.java
@@ -0,0 +1,15 @@
+package br.com.treinamento;
+
+public class Ternario {
+
+    public static void main(String[] args){
+        int a = 0;
+        int b = 3;
+        int c = 4;
+
+        a = b > c ? b : c;
+
+        System.out.println(a);
+    }
+    
+}

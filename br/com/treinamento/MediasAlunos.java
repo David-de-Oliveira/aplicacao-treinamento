6b82da7fc684b8780884e40853b94cc75c844e81
David Oliveira <david.deoliveirajunior@outlook.com>
2021-03-28 15:27:57 -0300

    Media alunos

---
+++ br/com/treinamento/MediasAlunos.java
@@ -0,0 +1,27 @@
+package br.com.treinamento;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class MediasAlunos {
+    public static void main(String[] args){
+        float notaAluno = 0;
+        float totalNota = 0;
+        float mediaNota = 0;
+        int totalAluno = 0;
+
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Insira um nota para continuar: ");
+            notaAluno = entrada.nextFloat();
+            totalNota += notaAluno;
+            totalAluno++;
+        }
+
+        mediaNota = totalNota / totalAluno;
+        System.out.println("a nota media da sala, para " + totalAluno +" alunos, foi de " + mediaNota);
+    }
+    
+}
